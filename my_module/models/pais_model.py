d5e1c8ca0485e9b02804b444356fd5a69c508f8b
andresmdgt <andres.miranda@cr.gt.com>
2021-06-21 13:48:55 -0600

    Update pais_model pais_view

--- my_module/models/pais_model.py
+++ my_module/models/pais_model.py
@@ -1,11 +1,20 @@
-import logging
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class Pais_Model(models.Model):
     _name = 'm.pais'
 
     nombre = fields.Char('Nombre')
+    codigo_pais = fields.Integer('Codigo Pais')
     nacionalidad = fields.Many2one('m.registro_salidas_pais', 'Registro de Salida')
     registro_salida = fields.One2many('m.pais', 'nacionalidad', string='Pais')
+
+    @api.constrains('codigo_pais')
+    def check_codigo_pais(self):
+        for codigo in self:
+            if not codigo.codigo_pais.isdigit():
+                raise ValidationError(_('Solo debe ingresar números.'))
+
+
 
     

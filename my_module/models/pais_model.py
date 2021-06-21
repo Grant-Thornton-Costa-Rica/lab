7af5b09863d1d560e63ef2c29c6d70f91e76e022
andresmdgt <andres.miranda@cr.gt.com>
2021-06-21 14:35:51 -0600

    Update pais_model.py

--- my_module/models/pais_model.py
+++ my_module/models/pais_model.py
@@ -3,15 +3,15 @@ from odoo.exceptions import ValidationError
 
 class Pais_Model(models.Model):
     _name = 'm.pais'
+    _sql_constraints = [
+        ('codigo_pais_unique', 'unique(codigo_pais)', 'El valor del código que desea ingresar ya existe, pruebe con uno distinto.')
+    ]
    
     nombre = fields.Char('Nombre')
     codigo_pais = fields.Integer('Codigo Pais')
     nacionalidad = fields.Many2one('m.registro_salidas_pais', 'Registro de Salida')
     registro_salida = fields.One2many('m.pais', 'nacionalidad', string='Pais')
 
-    _sql_constraints = [
-        ('codigo_pais_unique', 'unique('codigo_pais')', 'El valor del código que desea ingresar ya existe, pruebe con uno distinto.')
-    ]
 
 
 

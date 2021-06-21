397148641992ac41f6616b800a2b539e0536a493
andresmdgt <andres.miranda@cr.gt.com>
2021-06-21 16:06:42 -0600

    Update modelo registro

--- my_module/models/detalle_salidas_model.py
+++ my_module/models/detalle_salidas_model.py
@@ -2,10 +2,10 @@ import logging
 from odoo import models, fields, api
 
 class Detalle_Salidas_Model(models.Model):
-    _name = 'm.detalle_salidas'
+    _name = 'm.detalle'
 
     pais_visitado = fields.Many2one('m.pais', 'Pais Visitado')
     fecha_salida = fields.Datetime('Fecha Salida')
     fecha_entrada = fields.Datetime('Fecha Entrada')
     observaciones = fields.Char('Observaciones')
-    registro_salida = fields.Many2one('m.detalle_salidas', 'Detalle Salida')
+    registro_salida = fields.Many2one('m.detalle', 'Detalle Salida')
--- my_module/models/registro_salidas_model.py
+++ my_module/models/registro_salidas_model.py
@@ -2,10 +2,10 @@ import logging
 from odoo import models, fields, api
 
 class Registro_Salidas_Pais_Model(models.Model):
-    _name = 'm.registro_salidas_pais'
+    _name = 'm.registro'
 
     nombre = fields.Char('Nombre')
     fecha_nacimiento = fields.Date('Fecha Nacimiento')
     nacionalidad = fields.Many2one('m.pais', 'Nacionalidad')
-    registro_salida = fields.Many2one('m.detalle_salidas', 'Detalle Salida')
-    detalle_salida = fields.One2many('m.registro_salidas_pais', 'registro_salida', string='Registro Salidas Pais')
+    registro_salida = fields.Many2one('m.detalle', 'Detalle Salida')
+    detalle_salida = fields.One2many('m.registro', 'registro_salida', string='Registro Salidas Pais')

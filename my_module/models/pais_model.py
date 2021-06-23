89ec228c42c09aed3c0afb8965203ea844bf6904
andresmdgt <andres.miranda@cr.gt.com>
2021-06-23 14:13:05 -0600

    Update

--- my_module/models/pais_model.py
+++ my_module/models/pais_model.py
@@ -11,8 +11,9 @@ class Pais_Model(models.Model):
     codigo_pais = fields.Integer('Codigo')
     nombre = fields.Char('Nombre')
     nacionalidad = fields.Many2one('m.registro', 'Nacionalidad')
-    registro_salida = fields.One2many('m.detalle', 'pais_visitado', string='Pais')
+    registro_salida = fields.One2many('m.detalle', 'registro_salida', string='Pais')
     pais_visitado = fields.Many2one('m.detalle', 'Pais Visitado')
+
 
 
 

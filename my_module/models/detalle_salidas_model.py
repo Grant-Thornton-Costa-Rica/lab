66217afac3d7c57bb8bd3f7f7e3b4ee4dcf2ee29
andresmdgt <andres.miranda@cr.gt.com>
2021-06-23 15:51:03 -0600

    Update

--- my_module/models/detalle_salidas_model.py
+++ my_module/models/detalle_salidas_model.py
@@ -10,18 +10,18 @@ class Detalle_Salidas_Model(models.Model):
     registro_salida = fields.Many2one('m.registro', 'Detalle Salida')
 
     def _check_date(self, cr, uid, vals, context=None):
-    for obj in self.browse(cr, uid, ids):
-        start_date = obj.fecha_salida
-        end_date = obj.fecha_entrada
+        for obj in self.browse(cr, uid, ids):
+            start_date = obj.fecha_salida
+            end_date = obj.fecha_entrada
 
-        if start_date and end_date:
-            DATETIME_FORMAT = "%Y-%m-%d"
-            from_dt = datetime.datetime.strptime(start_date, DATETIME_FORMAT)
-            to_dt = datetime.datetime.strptime(end_date, DATETIME_FORMAT)
+            if start_date and end_date:
+                DATETIME_FORMAT = "%Y-%m-%d"
+                from_dt = datetime.datetime.strptime(start_date, DATETIME_FORMAT)
+                to_dt = datetime.datetime.strptime(end_date, DATETIME_FORMAT)
 
-            if to_dt < from_dt:
-                return False
-            return True
+                if to_dt < from_dt:
+                    return False
+        return True
 
     _constraints = [
         (_check_date, 'Your Message!', ['fecha_salida','fecha_entrada']),

3eb29cd38f5e2e89d6d9e38ae3c44589b807acdc
andresmdgt <andres.miranda@cr.gt.com>
2021-06-23 10:09:37 -0600

    Update

--- my_module/models/registro_salidas_model.py
+++ my_module/models/registro_salidas_model.py
@@ -6,5 +6,4 @@ class Registro_Salidas_Pais_Model(models.Model):
     nombre = fields.Char('Nombre')
     fecha_nacimiento = fields.Date('Fecha Nacimiento')
     nacionalidad = fields.Many2one('m.pais', 'Nacionalidad')
-    registro_salida = fields.Many2one('m.detalle', 'Detalle Salida')
-    detalle_salida = fields.One2many('m.registro', 'registro_salida', string='Registro Salidas Pais')
+    detalle_salida = fields.One2many('m.detalle', 'registro_salida', string='Registro Salidas Pais')

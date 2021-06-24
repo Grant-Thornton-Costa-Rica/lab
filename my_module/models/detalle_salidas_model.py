4ba4e858cc1d2a733c7612411a2ff03e72c25679
andresmdgt <andres.miranda@cr.gt.com>
2021-06-24 16:58:02 -0600

    update

--- my_module/models/detalle_salidas_model.py
+++ my_module/models/detalle_salidas_model.py
@@ -6,20 +6,18 @@ class Detalle_Salidas_Model(models.Model):
     _name = 'm.detalle'
     
     pais_visitado = fields.Many2one('m.pais', 'Pais Visitado')
-    fecha_salida = fields.Datetime('Fecha Salida')
-    fecha_entrada = fields.Datetime('Fecha Entrada')
+    fecha_salida = fields.Date('Fecha Salida')
+    fecha_entrada = fields.Date('Fecha Entrada')
     observaciones = fields.Char('Observaciones')
     registro_salida = fields.Many2one('m.registro', 'Detalle Salida')
 
-    dias = fields.Char(compute='_compute_calcular_dias', string='Dias transcurridos', store='True')
+    dias = fields.Char(compute='_compute_calcular_dias', string='Dias transcurridos')
     
     @api.depends('fecha_salida', 'fecha_entrada')
     def _compute_calcular_dias(self):
         if self.fecha_salida and self.fecha_entrada:
             for rec in self:
-                init_date = dt.strptime(rec.fecha_salida, '%Y-%m-%d')
-                end_date = dt.strptime(rec.fecha_entrada, '%Y-%m-%d')
-                rec.dias = str((end_date - init_date).days)
+                rec.dias = (rec.fecha_entrada - rec.fecha_salida).days
 
     @api.constrains('fecha_salida', 'fecha_entrada')
     def fecha_val(self):

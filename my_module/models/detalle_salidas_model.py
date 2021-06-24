839e3c8afbc60857fd556d2a984415fa4b2a74c5
andresmdgt <andres.miranda@cr.gt.com>
2021-06-24 16:45:17 -0600

    update

--- my_module/models/detalle_salidas_model.py
+++ my_module/models/detalle_salidas_model.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+from datetime import datetime
 
 class Detalle_Salidas_Model(models.Model):
     _name = 'm.detalle'
@@ -9,6 +10,18 @@ class Detalle_Salidas_Model(models.Model):
     fecha_entrada = fields.Datetime('Fecha Entrada')
     observaciones = fields.Char('Observaciones')
     registro_salida = fields.Many2one('m.registro', 'Detalle Salida')
+
+    dias = fields.Integer(compute='_compute_calcular_dias', 'Dias transcurridos')
+    
+    @api.depends('fecha_salida', 'fecha_entrada')
+    def _compute_calcular_dias(self):
+        datetime_format = '%Y-%m-%d %H:%M:%S'
+        fec_sal = datetime.datetime.strptime(fecha_salida, datetime_format)
+        fec_ent = datetime.datetime.strptime(fecha_entrada, datetime_format)
+        timedelta = fec_ent - fec_sal
+        dif_dias = timedelta.days + float(timedelta.seconds) / 86400
+
+        return dif_dias
 
     @api.constrains('fecha_salida', 'fecha_entrada')
     def fecha_val(self):

2dce18b4c853c3123c9993c9ae25ffd3cc49891e
andresmdgt <andres.miranda@cr.gt.com>
2021-06-25 15:12:02 -0600

    update

--- my_module/models/registro_salidas_model.py
+++ my_module/models/registro_salidas_model.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from datetime import date
 
 class Registro_Salidas_Pais_Model(models.Model):
     _name = 'm.registro'
@@ -7,6 +8,7 @@ class Registro_Salidas_Pais_Model(models.Model):
 
     nombre = fields.Char('Nombre')
     fecha_nacimiento = fields.Date('Fecha Nacimiento')
+    edad = fields.Char('Edad')
     nacionalidad = fields.Many2one('m.pais', 'Nacionalidad')
     direccion = fields.Char('Direccion')
     detalle_salida = fields.One2many('m.detalle', 'registro_salida', string='Registro Salidas Pais')
@@ -17,17 +19,13 @@ class Registro_Salidas_Pais_Model(models.Model):
     def _compute_nombre_pais(self):
         self.nombre_pais = self.nacionalidad.nombre
 
-    edad = fields.Char(compute='_compute_edad', string='')
-    
-    @api.depends('fecha_nacimiento')
-    def _compute_edad(self):
-        for rec in self:
-            if rec.fecha_nacimiento and rec.fecha_nacimiento <= fields.Date.today():
-                rec.age = relativedelta(
-                    fields.Date.from_string(fields.Date.today()),
-                    fields.Date.from_string(rec.fecha_nacimiento)
-                ).years
-            else:
-                rec.age = 0
+    def _obtener_edad(fecha_nacimiento: date) -> int:
+        hoy = date.today()
+        if fecha_nacimiento:
+            dif = int(fecha_nacimiento.replace(year = hoy.year) > hoy)
+            return date.hoy().year - fecha_nacimiento - dif
+        else:
+            return 0
+        
 
     

2933c683f6d45a3d9a7a73e29bbbf707bcdffe75
andresmdgt <andres.miranda@cr.gt.com>
2021-07-01 15:39:45 -0600

    update

--- my_module/models/registro_salidas_model.py
+++ my_module/models/registro_salidas_model.py
@@ -5,12 +5,14 @@ class Registro_Salidas_Pais_Model(models.Model):
     _name = 'm.registro'
     _description = 'Modulo Registro Salidas'
     _rec_name = 'nombre'
+    _check_company_auto = True
 
     nombre = fields.Char('Nombre')
     fecha_nacimiento = fields.Date('Fecha Nacimiento')
     edad = fields.Char(compute='_compute_edad', string='Edad')
     nacionalidad = fields.Many2one('m.pais', 'Nacionalidad')
-    company_id = fields.Many2one('res.company', 'Company', required=True, readonly=False, default=lambda self: self.env.company.id)
+    company_id = fields.Many2one('res.company', 'Company', required=True, readonly=True, default=lambda self: self.env.company)
+    other_record_id = fields.Many2one('other.record', check_company=True)
     direccion = fields.Char('Direccion')
     detalle_salida = fields.One2many('m.detalle', 'registro_salida', string='Registro Salidas Pais')
 

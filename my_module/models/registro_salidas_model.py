56c89fec2571621a145465e68f8be82dd4092594
andresmdgt <andres.miranda@cr.gt.com>
2021-07-01 09:42:56 -0600

    update

--- my_module/models/registro_salidas_model.py
+++ my_module/models/registro_salidas_model.py
@@ -10,7 +10,7 @@ class Registro_Salidas_Pais_Model(models.Model):
     fecha_nacimiento = fields.Date('Fecha Nacimiento')
     edad = fields.Char(compute='_compute_edad', string='Edad')
     nacionalidad = fields.Many2one('m.pais', 'Nacionalidad')
-    partner_id = fields.Many2one('res.company', 'Compañia')
+    company = fields.Many2one('res.company', 'Company', default=lambda self: self.env.company_id.id if self.payment_id == 'outbound' else None)
     direccion = fields.Char('Direccion')
     detalle_salida = fields.One2many('m.detalle', 'registro_salida', string='Registro Salidas Pais')
 

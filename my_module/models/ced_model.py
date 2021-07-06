ba7273773cdad2d3e5ff3b587aeabe414005e957
andresmdgt <andres.miranda@cr.gt.com>
2021-07-06 11:04:54 -0600

    update

--- my_module/models/ced_model.py
+++ my_module/models/ced_model.py
@@ -3,6 +3,6 @@ from odoo import models, fields, api
 class Tipo_Cedula_Model(models.Model):
     _name = 'm.cedula'
     _description = 'Modulo Tipo Cedula'
-
+    
     ced = fields.Selection([('juridica', 'Cedula Juridica'), ('fisica', 'Cedula Fisica')], string="Tipo Cedula")
 

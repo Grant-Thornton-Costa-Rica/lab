21c52d6cb54e7ae834d0a5dc2fe1b22ab3086f5b
andresmdgt <andres.miranda@cr.gt.com>
2021-07-06 14:19:52 -0600

    update

--- my_module/models/ced_model.py
+++ my_module/models/ced_model.py
@@ -6,4 +6,9 @@ class Tipo_Cedula_Model(models.Model):
     _description = 'Modulo Tipo Cedula'
 
     ced = fields.Selection([('juridica', 'Cedula Juridica'), ('fisica', 'Cedula Fisica')], string="Tipo Cedula")
+    category_ids = fields.Many2many(
+      'res.company.category', 'cedula_category_rel',
+      'cedula_id', 'category_id',
+      string='Tags')
+     
 

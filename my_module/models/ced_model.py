c492ec06242c956aa666d241129f28fd9a2d04c0
andresmdgt <andres.miranda@cr.gt.com>
2021-07-06 15:48:06 -0600

    update

--- my_module/models/ced_model.py
+++ my_module/models/ced_model.py
@@ -1,14 +1,14 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 class Tipo_Cedula_Model(models.Model):
     _name = 'm.cedula'
     _inherit = 'res.company'
     _description = 'Modulo Tipo Cedula'
 
-    ced = fields.Selection([('juridica', 'Cedula Juridica'), ('fisica', 'Cedula Fisica')], string="Tipo Cedula")
-    category_ids = fields.Many2many(
-      'res.company.category', 'cedula_category_rel',
-      'cedula_id', 'category_id',
-      string='Tags')
+    # ced = fields.Selection([('juridica', 'Cedula Juridica'), ('fisica', 'Cedula Fisica')], string="Tipo Cedula")
+    # category_ids = fields.Many2many(
+    #   'res.company.category', 'cedula_category_rel',
+    #   'cedula_id', 'category_id',
+    #   string='Tags')
      
 

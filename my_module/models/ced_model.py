8bbc4eabff442f63da6bc93173a9760b4d3212c2
andresmdgt <andres.miranda@cr.gt.com>
2021-07-07 09:54:44 -0600

    update

--- my_module/models/ced_model.py
+++ my_module/models/ced_model.py
@@ -1,4 +1,5 @@
-from odoo import models, fields, api
+from odoo.exceptions import UserError
+from odoo import models, fields, api, _
 
 class Tipo_Cedula_Model(models.Model):
     _inherit = 'res.company'
@@ -6,5 +7,12 @@ class Tipo_Cedula_Model(models.Model):
 
     ced = fields.Selection([('juridica', 'Cedula Juridica'), ('fisica', 'Cedula Fisica')], string="Tipo Cedula")
 
-     
+@api.onchange('vat')
+def _onchange_ced(self):
+    for rec in self:
+        if rec.vat <= 9:
+            rec.ced = 'Cedula Fisica'
+        elif rec.vat > 9:
+            rec.ced = 'Cedula Juridica'
+
 

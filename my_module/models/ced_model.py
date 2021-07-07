e5661a9d0e436bbf6f3687259eb7d8f96ba72413
andresmdgt <andres.miranda@cr.gt.com>
2021-07-07 12:01:47 -0600

    update

--- my_module/models/ced_model.py
+++ my_module/models/ced_model.py
@@ -10,17 +10,17 @@ class Tipo_Cedula_Model(models.Model):
 @api.onchange('vat')
 def _onchange_ced(self):
     for rec in self:
-        if rec.vat <= 9:
+        if len(rec.vat) <= 9:
             rec.ced = 'Cedula Fisica'
-        elif rec.vat > 9:
+        elif len(rec.vat) > 9:
             rec.ced = 'Cedula Juridica'
 
 @api.constrains('vat','ced')
 def _constrains_fieldname(self):
     for rec in self:
-        if rec.vat >= 9 and rec.ced == 'juridica':
+        if len(rec.vat) >= 9 and rec.ced == 'juridica':
             raise UserError(_('La Cedula Fisica tiene que ser de 9 digitos'))
-        elif rec.vat < 11 and rec.ced == 'fisica':
+        elif len(rec.vat) < 11 and rec.ced == 'fisica':
             raise UserError(_('La Cedula Juridica tiene que ser de 11 digitos'))
 
 

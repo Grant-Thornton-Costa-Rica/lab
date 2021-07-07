0273bb4327c6fdb1ed47b4369180a45fb12cca8e
andresmdgt <andres.miranda@cr.gt.com>
2021-07-07 11:58:16 -0600

    update

--- my_module/models/ced_model.py
+++ my_module/models/ced_model.py
@@ -5,7 +5,7 @@ class Tipo_Cedula_Model(models.Model):
     _inherit = 'res.company'
     _description = 'Modulo Tipo Cedula'
 
-    ced = fields.Selection([('juridica', 'Cedula Juridica'), ('fisica', 'Cedula Fisica')], 'Type', string="Tipo Cedula")
+    ced = fields.Selection([('juridica', 'Cedula Juridica'), ('fisica', 'Cedula Fisica')], string="Tipo Cedula")
 
 @api.onchange('vat')
 def _onchange_ced(self):

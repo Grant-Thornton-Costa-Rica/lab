80778fe4fc8dca8a0d585ebd594c6a8de26a402c
andresmdgt <andres.miranda@cr.gt.com>
2021-07-12 15:52:59 -0600

    update

--- my_module/models/account_move_model.py
+++ my_module/models/account_move_model.py
@@ -17,12 +17,7 @@ class accountMoveModel(models.Model):
 
     def _check_cabys(self, lineas, obj = False):
         for rec in lineas:
-            test = (rec[0])
-            test1 = (rec[1])
-            test2 = (rec[2])
-            log.info("--------------------- " + str(test) + " ----------------------")
-            log.info("--------------------- " + str(test1) + " ----------------------")
-            log.info("--------------------- " + str(test2) + " ----------------------")
+            log.info("--------------------- " + str(rec) + " ----------------------")
             if obj:
                 cabys = rec.codigo_cabys
             else:

1c1eb02344ba861454163a049e1c75d19b322b78
andresmdgt <andres.miranda@cr.gt.com>
2021-07-12 10:02:42 -0600

    update

--- my_module/models/account_move_model.py
+++ my_module/models/account_move_model.py
@@ -11,7 +11,7 @@ class accountMoveModel(models.Model):
 
     @api.model
     def action_post(self):
-        if self.state == 'draft':
+        # if self.state == 'draft':
         # self._check_cabys(vals['invoice_line_ids'])
         super(accountMoveModel, self).action_post()
 

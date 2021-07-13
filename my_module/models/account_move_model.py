281d4da2df20003d6b8091a11dffec9ba0ffb8b3
andresmdgt <andres.miranda@cr.gt.com>
2021-07-13 12:57:05 -0600

    update

--- my_module/models/account_move_model.py
+++ my_module/models/account_move_model.py
@@ -34,13 +34,10 @@ class accountMoveModel(models.Model):
         self._check_cabys(vals['invoice_line_ids'])
         return super(accountMoveModel, self).create(vals)
 
-    @api.model
     def write(self, vals):
-        log.info("write: --------------------- " + str(vals) + " ----------------------")
-        cabys = self._check_cabys(vals['line_ids'])
-        for rec in self:
-            if cabys:
-                cabys
-        return super(accountMoveModel, self).write(vals)
+        res = super(accountMoveModel, self).write(vals)
+        for rec in res:
+            log.info("write: --------------------- " + rec.id + " ---------------------")
+            cabys = self._check_cabys(vals['line_ids'])
+        return res
 
-# --------------------- [[1, 4, {'price_unit': 336.75, 'credit': 336.75, 'amount_currency': -336.75, 'product_uom_id': False, 'product_id': False, 'tax_ids': [[6, False, []]], 'tax_base_amount': 2245, 'tax_tag_ids': [[6, False, []]], 'analytic_account_id': False, 'analytic_tag_ids': [[6, False, []]]}], [1, 5, {'price_unit': -2581.75, 'debit': 2581.75, 'amount_currency': 2581.75, 'date_maturity': '2021-08-31', 'product_uom_id': False, 'product_id': False, 'tax_ids': [[6, False, []]], 'tax_repartition_line_id': False, 'tax_tag_ids': [[6, False, []]], 'analytic_account_id': False, 'analytic_tag_ids': [[6, False, []]]}], [1, 6, {'tax_ids': [[6, False, [1]]], 'tax_repartition_line_id': False, 'tax_tag_ids': [[6, False, []]], 'analytic_account_id': False, 'analytic_tag_ids': [[6, False, []]]}], [1, 7, {'tax_ids': [[6, False, [1]]], 'tax_repartition_line_id': False, 'tax_tag_ids': [[6, False, []]], 'analytic_account_id': False, 'analytic_tag_ids': [[6, False, []]]}], [1, 8, {'tax_ids': [[6, False, [1]]], 'tax_repartition_line_id': False, 'tax_tag_ids': [[6, False, []]], 'analytic_account_id': False, 'analytic_tag_ids': [[6, False, []]]}], [0, 'virtual_824', {'account_id': 21, 'sequence': 11, 'name': '[E-COM08] Storage Box', 'quantity': 1, 'price_unit': 79, 'discount': 0, 'debit': 0, 'credit': 79, 'amount_currency': -79, 'date_maturity': False, 'currency_id': 2, 'partner_id': 14, 'product_uom_id': 1, 'product_id': 18, 'tax_ids': [[6, False, [1]]], 'tax_base_amount': 0, 'tax_exigible': True, 'tax_repartition_line_id': False, 'tax_tag_ids': [[6, False, []]], 'analytic_account_id': False, 'analytic_tag_ids': [[6, False, []]], 'recompute_tax_line': False, 'display_type': False, 'is_rounding_line': False, 'exclude_from_invoice_tab': False, 'codigo_cabys': '2325364'}]] ----------------------

885ccd00aba4b18780417bf6607956b3ebcd8ec7
andresmdgt <andres.miranda@cr.gt.com>
2021-07-14 14:00:01 -0600

    Modelo Identification Type

--- base_factura_electronica_v14/models/__init__.py
+++ base_factura_electronica_v14/models/__init__.py
@@ -2,8 +2,9 @@
 
 from . import res_company
 from . import activity_code
+from . import payment_method
 from . import res_country_canton
 from . import res_country_district
 from . import res_country_neighborhood
-from . import payment_method
+from . import identification_type
 from . import util_web_services

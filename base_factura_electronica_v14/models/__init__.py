b55c4ca3ea57ec57f156f30b08bf023dc417f467
andresmdgt <andres.miranda@cr.gt.com>
2021-07-15 08:36:00 -0600

    Update Modelo Contacts

--- base_factura_electronica_v14/models/__init__.py
+++ base_factura_electronica_v14/models/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from . import res_company
+# from . import res_company
 from . import activity_code
 from . import payment_method
-# from . import res_country_canton
-# from . import res_country_district
-# from . import res_country_neighborhood
+from . import res_country_canton
+from . import res_country_district
+from . import res_country_neighborhood
 from . import identification_type
 from . import util_web_services
